5cda719f3ca79efcda34666f2ae08dfdc2536718
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-15 14:18:10 +0200

    Add files via upload

---
+++ program1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Opgave1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kagePris = 23.56;
+            double ølPris = 34.67;
+            double pølsePris = 65.34;
+
+            double samletPris = kagePris + ølPris + pølsePris;
+
+            Console.WriteLine($"Kage: {kagePris:C2}");
+            Console.WriteLine($"Øl: {ølPris:C2}");
+            Console.WriteLine($"Pølse: {pølsePris:C2}");
+            Console.WriteLine($"I alt: {samletPris:C2}");
+        }
+    }
+}
