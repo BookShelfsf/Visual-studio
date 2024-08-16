66d4e3c495596e2d20a50c31e1799b96d72d31e9
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-16 09:34:05 +0200

    Add files via upload

---
+++ Program7.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double beløb = 250.50;
+            string indledning = "jeg har";
+            string afslutning = "kr. i banken.";
+            Console.WriteLine($"{indledning} {beløb} {afslutning}");
+            Console.ReadKey();
+        }
+    }
+}
