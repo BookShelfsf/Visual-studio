7c2714e77774a9486e0c35027aa752a929d49f51
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-16 13:01:34 +0200

    Add files via upload

---
+++ Program11.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VariabelEksempel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1 = 10;
+            int var2 = 20;
+
+            bool erVar1StørreEndVar2 = var1 > var2;
+
+            Console.WriteLine($"Er var1 større end var2? {erVar1StørreEndVar2}");
+            Console.ReadKey();
+            var1 = 30;
+            
+            erVar1StørreEndVar2 = var1 > var2;
+            Console.WriteLine($"Er var1 større end var2? {erVar1StørreEndVar2}");
+            Console.ReadKey ();
+        }
+    }
+}
