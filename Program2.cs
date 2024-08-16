2701372b6eb7074ab0c9e2b6cba455e4fb021bd4
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-16 07:41:00 +0200

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast dit navn");
+            string navn = Console.ReadLine();
+
+            Console.Write("Indtast din alder");
+            int alder = int.Parse(Console.ReadLine());
+            Console.WriteLine($"jeg hedder {navn}, og er {alder} år gammel");
+            Console.ReadKey();
+        }
+    }
+}
