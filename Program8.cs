3543e2170b3cc26b665649d18e5ff3f691e67c0a
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-16 10:18:45 +0200

    Add files via upload

---
+++ Program8.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double regn = 2 + 1 * 2;
+            double regn1 = (2 + 1) * 2;
+            double regn2 = 5.0 / 2;
+            double regn3 = 8 % 3;
+            double regn4 = 1 - 5;
+
+            Console.WriteLine("{0}", regn);
+            Console.WriteLine("{0}", regn1);
+            Console.WriteLine("{0}", regn2);
+            Console.WriteLine("{0}", regn3);
+            Console.WriteLine("{0}", regn4);
+            Console.ReadKey();
+        }
+    }
+}
