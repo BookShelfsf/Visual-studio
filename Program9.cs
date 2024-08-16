54efebdcd9b7389c949f44e8491f835db132b2c0
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-16 10:35:50 +0200

    Add files via upload

---
+++ Program9.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int plus = 12;
+            int minus = 6;
+            int gange = 3;
+            int resultat = (plus - minus) * gange;
+            Console.WriteLine("(12 - 6) * 3 = {0}", resultat);
+            Console.ReadKey();
+        }
+    }
+}
