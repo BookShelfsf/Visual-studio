5c217995b66459d7404fc295553052bf676bf3eb
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-16 07:53:27 +0200

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast Radius af cirklen");
+            double radius = double.Parse(Console.ReadLine());
+            double areal = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine("Hvis radius af cirklen er {0} arealen af cirklen {1}", radius, areal);
+            Console.ReadKey();
+        }
+    }
+}
