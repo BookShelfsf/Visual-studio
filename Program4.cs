123887d35b192f45f2a5c790c959495b1339feaa
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-16 08:05:51 +0200

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst = "hej, Verden";
+            int heltal = 42;
+            double decimaltal = 3.14;
+
+            Console.WriteLine(tekst);
+            Console.ReadKey();
+            Console.WriteLine(heltal);
+            Console.ReadKey();
+            Console.WriteLine(decimaltal);
+            Console.ReadKey();
+        }
+    }
+}
