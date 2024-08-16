7aec87e7d5663e8800365e22c785de5b966fe617
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-16 08:27:30 +0200

    Add files via upload

---
+++ Program6.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dato = "I dag har vi den 24. December";
+            Console.WriteLine(dato);
+            Console.ReadKey();
+
+
+        }
+    }
+}
