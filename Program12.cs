c81215a34d65ec28d22681821746776747451b0f
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-16 13:47:30 +0200

    Add files via upload

---
+++ Program12.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal1 = 42;
+            int tal2 = 64;
+            int result = tal1 + tal2;
+
+            if (result > 100)
+            {
+                Console.WriteLine("Summen er over 100");
+            }
+            else if (result < 100)
+            {
+                Console.WriteLine("Summen er under 100");
+            }
+            else if (result == 100)
+            {
+                Console.WriteLine("summen er 100");
+            }
+        }
+    }
+}
