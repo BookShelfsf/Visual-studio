0a01b11213f2a72853ccf935c4293fb08e1db72b
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-19 12:40:33 +0200

    Add files via upload

---
+++ Program15.cs
@@ -0,0 +1,22 @@
+﻿
+            Console.WriteLine("Her kan du skrive navn, brugernavn og password");
+            Console.ReadKey();
+
+            Console.WriteLine("Skriv navn her");
+            string Navn = Console.ReadLine();
+            Console.WriteLine("Skriv brugernavn her");
+            string brugernavn = Console.ReadLine();
+            Console.WriteLine("Skriv pasword her");
+            string password = Console.ReadLine();
+
+            string korrektBrugernavn = "LukasHF";
+            string korrektPassword = "Homse2008";
+
+            if (brugernavn == korrektBrugernavn && password == korrektPassword)
+            {
+             Console.WriteLine("Velkommen, " + Navn + "!");
+            }
+            else
+            {   
+             Console.WriteLine("Brugernavn eller password er forket");
+            }
