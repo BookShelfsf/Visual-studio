f4e5b3c62ab42924a6374a0fd97f174877aa1e7a
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-19 14:46:03 +0200

    Add files via upload

---
+++ Program16.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Her kan du skrive brugernavn");
+        Console.ReadKey();
+
+        string korrektBrugernavn = "LukasHF";
+        string korrektPassword = "Homse2008";
+
+        Console.WriteLine("Skriv brugernavn her");
+        string brugernavn = Console.ReadLine();
+
+        if (brugernavn == korrektBrugernavn)
+        {
+            Console.WriteLine("Skriv password her");
+            string password = Console.ReadLine();
+            if (password == korrektPassword)
+            {
+                Console.WriteLine("Skriv dit navn her");
+                string navn = Console.ReadLine();
+                Console.WriteLine("Velkommen, " + navn + "!");
+            }
+            else
+            {
+                Console.WriteLine("Passwordet er forkert.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Brugernavnet er forkert.");
+        }
+    }
+}
