cf80a44050b7313445927987b536c2b188d7874b
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-19 08:20:03 +0200

    Add files via upload

---
+++ Program14.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Indtast din alder: ");
+
+            string input = Console.ReadLine();
+
+            int alder;
+
+            // Forsøger at konvertere input til en integer
+            if (int.TryParse(input, out alder))
+            {
+                if (alder > 60)
+                {
+                    Console.WriteLine("Du er for gammel");
+                }
+                else if (alder >= 50)
+                {
+                    Console.WriteLine("Du er hverken for gammel eller for ung");
+                }
+                else
+                {
+                    Console.WriteLine("Du er for ung");
+                }
+            }
+            else
+            {
+                // Hvis input ikke kan konverteres til et heltal, vises en fejlbesked
+                Console.WriteLine("Ugyldigt input. Indtast venligst et tal.");
+            }
+        }
+    }
+}
