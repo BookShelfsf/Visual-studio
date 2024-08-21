e59da277d91a3557c5a46fbb1f2f8e2958c72525
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-21 10:10:35 +0200

    Add files via upload

---
+++ Program23.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 100; i >= 51; i--)
+        {
+            Console.WriteLine(i);
+        }
+        int j = 50;
+        while (j >= 1)
+        {
+            Console.WriteLine(j);
+            j--;
+        }
+    }
+}
