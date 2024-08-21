1e4e11f129bdf483e61454a49595cf674a753355
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-21 13:27:43 +0200

    Add files via upload

---
+++ Program34.cs
@@ -0,0 +1,18 @@
+﻿int i = 1;
+
+while (i <= 10)
+{
+    int resultat = i * 4;
+
+    if (resultat == 16)
+    {
+        i++;
+        continue;
+    }
+
+    Console.WriteLine(resultat);
+    i++;
+}
+
+Console.WriteLine("Loopen er nu stoppet.");
+Console.ReadKey();
