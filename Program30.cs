e1b17967aaef4d8dcfcdbde9fc388b6d7f0eaed1
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-21 12:36:01 +0200

    Add files via upload

---
+++ Program30.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int startX = 20;
+        int startY = 4;
+        int endX = 30;
+        int endY = 8;
+
+        for (int x = startX; x <= endX; x++)
+        {
+            Console.SetCursorPosition(x, startY);
+            Console.Write('*');
+        }
+
+        for (int x = startX; x <= endX; x++)
+        {
+            Console.SetCursorPosition(x, endY);
+            Console.Write('*');
+        }
+
+        for (int y = startY; y <= endY; y++)
+        {
+            Console.SetCursorPosition(startX, y);
+            Console.Write('*');
+        }
+
+        for (int y = startY; y <= endY; y++)
+        {
+            Console.SetCursorPosition(endX, y);
+            Console.Write('*');
+        }
+
+        string name = "lukas"; 
+        int nameLength = name.Length;
+
+        int centerX = (startX + endX - nameLength) / 2;
+        int centerY = (startY + endY) / 2;
+
+        Console.SetCursorPosition(centerX, centerY);
+        Console.Write(name);
+
+        Console.ReadKey();
+    }
+}
