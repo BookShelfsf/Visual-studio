dc88fee94df111ed0364caa34c4a0b2681d6daa4
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-08-21 12:26:26 +0200

    Add files via upload

---
+++ Program28.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Indtast et tal for at udskrive tabellen: ");
+
+        int tal = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{tal} x {i} = {tal * i}");
+        }
+        Console.ReadKey();
+    }
+}
